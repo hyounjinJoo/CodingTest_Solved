d09727a964def94f60774f5dd8eb613273176bb9
Hyoun Jin Joo ( 주현진 ) <hyounjin.dev@gmail.com>
2023-10-31 10:41:24 +0900

    solved.ac의 Class1으로 문제풀이집을 변경, 문제 1152번 해결

---
+++ Class/Class1/Q1152.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cstring>
+
+int main()
+{
+	char str[1000001];
+	std::cin.getline(str, 1000001);
+
+	size_t length = std::strlen(str);
+
+	int countWord = 0;
+	size_t index = 0;
+	if (' ' == str[index])
+	{
+		++index;
+	}
+
+	for (; index < length; ++index)
+	{
+		if (' ' == str[index] || index == (length - 1))
+		{
+			++countWord;
+		}
+	}
+
+	std::cout << countWord;
+
+	return 0;
+}
